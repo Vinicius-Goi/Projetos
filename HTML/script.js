03ab707da1b5f3569e107171a5ee1900b91457ff
Goi <126573272+Vinicius-Goi@users.noreply.github.com>
2023-12-26 22:45:45 -0300

    test

---
+++ HTML/script.js
@@ -0,0 +1,6 @@
+function enviarFormulario(){
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+
+    document.getElementById('form').reset();
+}
